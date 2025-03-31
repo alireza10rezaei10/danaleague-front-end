24d1055ddaa38acd1838a0e63f0e80e80bccce16
Alireza Rezaei <alireza10rezaei10@gmail.com>
2025-03-31 05:44:35 +0330

    make chess ground component more reusable

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
       <br />
       <br />
       <br />
-      <ChessGround />
+      <ChessGround colors={["bg-white", "bg-gray-300"]} number_of_rows={2} />
       <br />
       <br />
       <br />
--- components/ChessGround/ChessGround.jsx
+++ components/ChessGround/ChessGround.jsx
@@ -1,15 +1,16 @@
 "use client";
 
-const ChessGround = ({ colors }) => {
-  const numberOfSquares =
+const ChessGround = ({ colors, number_of_rows }) => {
+  const number_of_squares =
     Math.floor((window.innerWidth + window.outerWidth) / 50) + 1;
 
-  const squares = Array.from({ length: numberOfSquares });
+  const squares = Array.from({ length: number_of_squares });
+  const rows = Array.from({ length: number_of_rows });
 
   return (
     <div className="w-full h-full flex justify-center items-center overflow-x-hidden">
       <div>
-        {[0, 1].map((rowIndex) => (
+        {rows.map((_, rowIndex) => (
           <div key={rowIndex} className="flex w-max">
             {squares.map((_, index) => {
               const colorIndex = (index + rowIndex) % 2; // Alternate colors between rows
