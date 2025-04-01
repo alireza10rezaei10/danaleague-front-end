5aa20fdd6c9953942c66f46ca61db73d027c540c
Alireza Rezaei <alireza10rezaei10@gmail.com>
2025-04-01 20:48:05 +0330

    making footer

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Hero from "./sections/Hero";
 import Intro from "./sections/Intro";
 import Last from "./sections/Last";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -8,60 +9,7 @@ export default function Home() {
       <Hero />
       <Intro />
       <Last />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+      <Footer />
     </div>
   );
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer className="mt-30 h-50 bg-[var(--primary-color)]"></footer>;
+};
+
+export default Footer;
